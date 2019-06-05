403088a73309cb6d8ffc14dc22eec026ee76f14a
Olli Laitinen <laitinenolli@outlook.com>
2019-06-06 00:58:51 +0300

    Login form builded Create account form builded Database builded

---
+++ MonitoringTool/Form3.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonitoringTool
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-9SB5A08\MSSQLSERVER01;Integrated Security=True;Connect Timeout=30;Encrypt=False;
+                                                     TrustServerCertificate=False;Initial Catalog=TIETOKANTA;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            //Inserts data to sql table
+            SqlCommand insert = new SqlCommand("insert into [ACCOUNTS] (USERNAME, PASSWORD,FirstName,LastName) values(@USERNAME, @PASSWORD,@FirstName,@LastName)", conn);
+            insert.Parameters.AddWithValue("@USERNAME", textBox1.Text);
+            insert.Parameters.AddWithValue("@PASSWORD", textBox2.Text);
+            insert.Parameters.AddWithValue("@FirstName", textBox3.Text);
+            insert.Parameters.AddWithValue("@LastName", textBox4.Text);
+
+            if (textBox1.Text == string.Empty)
+            {
+                textBox1.Text = "You need add username!";
+                return;
+            }
+            else if (textBox2.Text == string.Empty)
+            {
+                textBox2.Text = "You need to add password!";
+            }
+            else if (textBox3.Text == string.Empty)
+            {
+                textBox3.Text = "You need to add firstname";
+            }
+            else
+            {
+                conn.Open();
+                insert.ExecuteNonQuery();
+                MessageBox.Show("Register done !");
+            }
+
+        }
+    }
+}
--- MonitoringTool/Properties/Settings.Designer.cs
+++ MonitoringTool/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MonitoringTool.Properties
-{
-
-
+namespace MonitoringTool.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.1.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-9SB5A08\\MASTERBUJI;Initial Catalog=TIETOKANTA;Integrated Secu" +
+            "rity=True")]
+        public string TIETOKANTAConnectionString {
+            get {
+                return ((string)(this["TIETOKANTAConnectionString"]));
             }
         }
     }
